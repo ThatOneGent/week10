41b60afdd6b0a2af121619a813ca7899f780e613
Markus <119375162+ThatOneGent@users.noreply.github.com>
2023-03-10 23:38:56 -0500

    first commit of project

---
+++ table.js
@@ -0,0 +1,51 @@
+const tableHeaders = [
+    "First Name",
+    "Last Name",
+    "E-mail",
+    "City",
+    "State",
+    "pin"
+];
+
+let tableInfo = [
+    {
+        "fname": "Connor",
+        "lname": "Flowers",
+        "email": "mauris.ipsum@protonmail.org",
+        "city": "Chandler",
+        "state": "AZ",
+        "pin": 3800
+    },
+    {
+        "fname": "Hall",
+        "lname": "Rhodes",
+        "email": "metus.sit.amet@hotmail.net",
+        "city": "San Jose",
+        "state": "CA",
+        "pin": 8474
+    },
+    {
+        "fname": "Kennedy",
+        "lname": "Blake",
+        "email": "et@aol.com",
+        "city": "Mobile",
+        "state": "AL",
+        "pin": 8858
+    },
+    {
+        "fname": "Jessamine",
+        "lname": "Perkins",
+        "email": "dolor@protonmail.ca",
+        "city": "Gulfport",
+        "state": "MS",
+        "pin": 6635
+    },
+    {
+        "fname": "Nissim",
+        "lname": "Morton",
+        "email": "odio.aliquam@google.com",
+        "city": "Lakewood",
+        "state": "CO",
+        "pin": 7642
+    }
+];
